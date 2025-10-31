85e7d62499f485c5f1b45193823a88943f4b3546
Bassi <afroboost@mac.home>
2025-10-31 16:09:30 +0100

    Fix next.config.js syntax and remove TS version

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: ['res.cloudinary.com'],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    serverActions: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
